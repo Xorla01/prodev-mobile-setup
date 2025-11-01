78ab7955382fd0ac6ee9becb99c15fc763919758
Cowther Richmond <richmondcowther@gmail.com>
2025-11-01 15:06:56 +0000

    #Update

--- prodev-mobile-app-0x01/app/index.tsx
+++ prodev-mobile-app-0x01/app/index.tsx
@@ -2,13 +2,13 @@ import { Text, View, StyleSheet } from "react-native";
 
 export default function Index() {
   return (
-    <View style={ styles.container }>
+    <View style={styles.container}>
       <Text>Entry Screen - Awesome</Text>
 
       <View>
-        <Text style={ styles.largeText}>Typescript is great if you practice more</Text>
-        <Text style={ styles.mediumText}>React Native provides you a single codebase for cross platforms</Text>
-        <Text style={ styles.smallText}>ALX is awesome</Text>
+        <Text style={styles.largeText}>TypeScript is great if you practice more</Text>
+        <Text style={styles.mediumText}>React Native provides you a single codebase for cross platforms</Text>
+        <Text style={styles.smallText}>ALX is awesome</Text>
       </View>
     </View>
   );
@@ -16,26 +16,29 @@ export default function Index() {
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
     backgroundColor: "#90caf9",
+    alignItems: "center",
+    justifyContent: "center",
   },
   largeText: {
     fontSize: 30,
     color: "#f44336",
     marginBottom: 5,
-    fontWeight: 700,
+    fontWeight: "700",
     fontVariant: ["small-caps"],
   },
   mediumText: {
     fontSize: 30,
     color: "#9c27b0",
     marginBottom: 10,
-    fontWeight: 500,
+    fontWeight: "500",
     textAlign: "right",
   },
   smallText: {
     fontSize: 30,
     color: "#2196f3",
-    fontWeight: 500,
+    fontWeight: "500",
     textAlign: "center",
-  }
+  },
 });
