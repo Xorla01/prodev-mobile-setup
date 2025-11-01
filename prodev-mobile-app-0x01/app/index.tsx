abb3130bf7577825b90a9aa858c5920d40d99407
Cowther Richmond <richmondcowther@gmail.com>
2025-11-01 15:03:42 +0000

    #Update

--- prodev-mobile-app-0x01/app/index.tsx
+++ prodev-mobile-app-0x01/app/index.tsx
@@ -16,9 +16,6 @@ export default function Index() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
     backgroundColor: "#90caf9",
   },
   largeText: {
